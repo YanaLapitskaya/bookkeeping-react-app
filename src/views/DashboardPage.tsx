48c50400afc923c54412e718bafff2fd8a0d16a6
Yana <yanalapitskay@gmail.com>
2017-12-19 02:01:11 +0100

    moved routes from App level to Dashboard Page

--- src/views/DashboardPage.tsx
+++ src/views/DashboardPage.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
 import TransactionPanel from './TransactionPanel';
 import Header from './Header';
+import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import TransactionEditPage from './TransactionEditPage';
+import TransactionDetailsPage from './TransactionDetailsPage';
 
 export default class DashboardPage extends React.Component {
     render() {
         return (
             <div>
                 <Header/>
-                <TransactionPanel/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact={true} path="/dashboard" render={() => <TransactionPanel />} />
+                        <Route
+                            exact={true}
+                            path={`/transaction/:id`}
+                            render={
+                                ({match: {params: {id}}}) => {
+                                    return   <TransactionDetailsPage id={id} match={null} location={null} history={null} />;
+                                }}
+                        />
+                        <Route
+                            exact={true}
+                            path={`/transaction/:id/edit`}
+                            render={
+                                ({match: {params: {id}}}) => {
+                                    return <TransactionEditPage id={id} match={null} location={null} history={null} />;
+                                }}
+                        />
+                    </Switch>
+                </BrowserRouter>
             </div>
         );
     }
