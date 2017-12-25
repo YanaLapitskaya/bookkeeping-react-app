5460ea460536a4146b2d3a5a2bb635c42515ba7b
Yana <yanalapitskay@gmail.com>
2017-12-25 18:10:16 +0100

    added image icons and ability to view image in new window on Transaction panel

--- src/views/transaction/TransactionTable.tsx
+++ src/views/transaction/TransactionTable.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import Transaction from '../../models/Transaction';
 import Card from '../../models/Card';
 import { Link } from 'react-router-dom';
+import { HOST } from '../../Constants';
 
 interface TableProps {
     trans: Array<Transaction>;
     cards: Array<Card>;
 }
+
 export default class TransactionTable extends React.Component<TableProps, {}> {
     render() {
         let trans = this.props.trans;
@@ -25,6 +27,7 @@ export default class TransactionTable extends React.Component<TableProps, {}> {
                         <th scope="col">Amount</th>
                         <th scope="col">Card</th>
                         <th scope="col">Card balance</th>
+                        <th scope="col">Check</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -42,7 +45,25 @@ interface RowProps {
     key: Number;
     num: Number;
 }
+
 class TransactionRow extends React.Component<RowProps, {}> {
+    onImageClick() {
+        let checkPath;
+        if (this.props.tran.check) {
+            checkPath = HOST + this.props.tran.check.replace(/public/i, '');
+        }
+
+        if (checkPath) { this.popupCenter(checkPath, 'Check', 300, 230); }
+    }
+
+    popupCenter(url: string, title: string, w: any, h: any) {
+        let left = (screen.width / 2) - (w / 2);
+        let top = (screen.height / 2) - (h / 2);
+        return window.open(url, title, 'toolbar=no, location=no, directories=no,' +
+            ' status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, ' +
+            'width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+    }
+
     render() {
         let card = this.props.cards.filter((c: any) => {return c.id === this.props.tran.card; })[0];
         let classTr = this.props.tran.amount < 0 ? 'table-danger' : 'table-success';
@@ -55,6 +76,11 @@ class TransactionRow extends React.Component<RowProps, {}> {
                 <td>{this.props.tran.amount > 0 ? '+' : ''}{this.props.tran.amount}</td>
                 <td>{card && card.paymentSystem + ': ' + card.number}</td>
                 <td>{card && card.amount}</td>
+                <td>
+                    {this.props.tran.check ?
+                    <input type="image" onClick={() => {this.onImageClick(); }} src={require('./../../img-icon.png')}/>
+                        : null}
+                </td>
                 <td>
                     <Link to={`/dashboard/transaction/${this.props.tran.id}`}>
                         Details >
